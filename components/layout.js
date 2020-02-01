9c5c36d1ae581d8b159f0c90f201df7c7c7c4cb9
efdali <efdal_incesu@hotmail.com>
2020-02-01 23:57:27 +0300

    favicon change

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => (
   <>
     <Head>
       <meta charSet="utf-8" />
-      <link rel="icon" href="/footer.png" />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <title>Mükemmel Blog</title>
